a33d70e43748e07c6cb8a5d22918bf1349955e78
shurupats <shurupats@gmail.com>
2023-02-08 22:33:10 +0300

    Преобразование в массив

--- js/main.js
+++ js/main.js
@@ -6,9 +6,10 @@ function searchGif(){
 }
 
 async function getGif(gifName){
+  let result = [];
   let str = `https://api.giphy.com/v1/gifs/search?api_key=h0PbnSCpetJD41tHZQZVTRP2zqETXR6U&q=` + gifName + `&limit=5&offset=0&rating=g&lang=en`;
   let response = await fetch(str);
-  let result = await response.json();
+  result = await response.json().data;
 
   return result;
 }
