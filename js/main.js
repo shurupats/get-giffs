18426fb85ae210bbfc5d0c01aa1643a72ecfd092
shurupats <shurupats@gmail.com>
2023-02-12 19:57:04 +0300

    починила поиск

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
 
-function searchGif(){
-  let status = document.querySelector(".container-search__field").value;
-  let cat = getGif(status);
+async function searchGif(){
+  const searchField = document.querySelector(".container-search__field");
+  const gifName  = searchField.value;
+  searchField.value = '';
+  getGif(gifName, 5).then((data) => printGifs(data));
 
 }
 
-async function getGif(gifName){
-  let result = [];
-  let str = `https://api.giphy.com/v1/gifs/search?api_key=h0PbnSCpetJD41tHZQZVTRP2zqETXR6U&q=` + gifName + `&limit=5&offset=0&rating=g&lang=en`;
-  let response = await fetch(str);
-  result = await response.json().data;
+function printGifs(result){
+  const gifs = result.data;
+  const resultContainer = document.querySelector('.container-result');
+  clearResultContainer(resultContainer);
+  gifs.forEach(gif => {
+    let gifContainer = document.createElement('img');
+    gifContainer.className = 'container-result__gif';
+    gifContainer.src = gif.images.downsized.url;;
+    resultContainer.appendChild(gifContainer);
+  });
+}
 
-  return result;
+function clearResultContainer(container){
+  container.innerHTML = '';
+}
+
+async function getGif(gifName, limit){
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=h0PbnSCpetJD41tHZQZVTRP2zqETXR6U&q=${gifName}&limit=${limit}&offset=0&rating=g&lang=en`;
+  const response = await fetch(url);
+  return await response.json();
 }
 
 document.querySelector(".button").addEventListener("click", searchGif);
